bf71a7f8c19a153a51141b78a83f0b2316f21ab5
Andrew Ong <andrew.hai.ong@gmail.com>
2023-08-09 19:51:55 +1000

    Adding knockd

--- clean-up.sh
+++ clean-up.sh
@@ -6,27 +6,29 @@ export BUILD_DIR=$HOME/l3a
 apt-get install -y knockd
 cat << EOF > /etc/knockd.conf
 [options]
-	UseSyslog
-	Interface = bond0
+  UseSyslog
+  Interface = bond0
 
 [openSSH]
-	sequence    = 700:udp,707:udp,777:udp
-	seq_timeout = 2
-	start_command = /sbin/iptables -A INPUT -s %IP% -p tcp --dport 22 -j ACCEPT
-	stop_command = /sbin/iptables -D INPUT -s %IP% -p tcp --dport 22 -j ACCEPT
-	tcpflags    = syn,ack
+  sequence    = 700:udp,707:udp,777:udp
+  seq_timeout = 2
+  start_command = /sbin/iptables -A INPUT -s %IP% -p tcp --dport 22 -j ACCEPT
+  stop_command = /sbin/iptables -D INPUT -s %IP% -p tcp --dport 22 -j ACCEPT
+  tcpflags    = syn,ack
 
 [kubectl]
-	sequence    = 600:udp,606:udp,666:udp
-	seq_timeout = 2
-	start_command = /sbin/iptables -A INPUT -s %IP% -p tcp --dport 6443 -j ACCEPT
-	stop_command = /sbin/iptables -D INPUT -s %IP% -p tcp --dport 6443 -j ACCEPT
-	tcpflags    = syn,ack
+  sequence    = 600:udp,606:udp,666:udp
+  seq_timeout = 2
+  start_command = /sbin/iptables -A INPUT -s %IP% -p tcp --dport 6443 -j ACCEPT
+  stop_command = /sbin/iptables -D INPUT -s %IP% -p tcp --dport 6443 -j ACCEPT
+  cmd_timeout = 600
+  tcpflags    = syn,ack
+
 [closeSSH]
-	sequence    = 900:udp,909:udp,999:udp
-	seq_timeout = 5
-	command     = /sbin/iptables -D INPUT -s %IP% -p tcp --dport 2380 -j ACCEPT
-	tcpflags    = syn
+  sequence    = 900:udp,909:udp,999:udp
+  seq_timeout = 5
+  command     = /sbin/iptables -D INPUT -s %IP% -p tcp --dport 22 -j ACCEPT
+  tcpflags    = syn
 EOF
 
 systemctl enable knockd && \
